41c478bc0f628164fae47d0cea6b555721c97417
Raspberry Pi <14131524+jotunnch@users.noreply.github.com>
2020-03-17 19:39:13 -0700

    learned how to use expressions to modify values in variables

--- ch_03_building_traveling_math/block_stack.py
+++ ch_03_building_traveling_math/block_stack.py
@@ -1,7 +1,22 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
-x = 16,
+x = 16
 y = 0
 z = -5
 block_type = 103
+mc.setBlock(x, y, z, block_type)
+
+# stack em
+y = y + 1
+block_type = block_type - 40
+mc.setBlock(x, y, z, block_type)
+
+# stack em
+y = y + 1
+block_type = block_type - 100
+mc.setBlock(x, y, z, block_type)
+
+# stack em
+block_type = block_type + 40
+y = y + 1
 mc.setBlock(x, y, z, block_type)
